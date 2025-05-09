b183598606e03d4b17d513c2feafa88f8291d2c5
Sushant Kumar <sushiitantmi45@gmail.com>
2025-05-10 01:33:44 +0530

    Updated Deployment

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,6 +1,14 @@
 export const projectsData = [
   {
     id: 1,
+    name: "All-About SSK",
+    description: "This is the website dedicated to Sushant Kumar(Ssk). SSK-DRAKE AI-Chatbot integrated with it. This Website is made using next.js",
+    date: "2025-05-10",
+    link:"https://all-about-ssk-nitsush45s-projects.vercel.app/",
+    image:"/background/all-about-ssk.png",
+  },
+  {
+    id: 2,
     name: "Ssk-ChatMaster",
     description:
       "Ssk-ChatMaster is a modern one-to-one client-server chat application built using cutting-edge technologies. It is fully encrypted, ensuring secure communication, and comes with an integrated AI bot for personal assistance. The app includes various features such as blocking users, changing avatars, emojis, and personal support, providing an engaging and secure chatting experience.",
@@ -9,7 +17,7 @@ export const projectsData = [
     image:"/background/sskchat.png",
   },
   {
-    id: 2,
+    id: 3,
     name: "CRED-Project",
     description:
       "This is a Flutter-based project named CredProj. The project is designed to demonstrate key Flutter features and functionality.User-friendly interface.Smooth navigation between screens.Responsive design for different screen sizes.Optimized for release builds.",
@@ -18,7 +26,7 @@ export const projectsData = [
     image: "/background/credproj.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "Video-Description generator",
     description:
       "This project automates the process of downloading videos, uploading them to the Socialverse platform, and creating posts with the uploaded videos. It uses Python for handling video download, upload, and post creation. The bot monitors a local directory for new .mp4 video files and processes them accordingly.",
@@ -27,7 +35,7 @@ export const projectsData = [
     image: "/background/vidcap.png",
   },
   {
-    id: 4,
+    id: 5,
     name: "AI-ChatBot",
     description: "Interactive AI Bot made with the help of BotPress.",
     date: "2024-10-19",
@@ -35,7 +43,7 @@ export const projectsData = [
     image: "/background/bot.png",
   },
   {
-    id: 5,
+    id: 6,
     name: "Relationship-Care",
     description:
       "Interactive UI app containing many blogs regarding the relationship along with booking consultation with time and date interface.",
@@ -44,7 +52,7 @@ export const projectsData = [
     image: "/background/ssk-rel.png",
   },
   {
-    id: 6,
+    id: 7,
     name: "Dinner Project",
     description:
       "Interactive UI app containing many blogs regarding the relationship along with booking consultation with time and date interface.",
@@ -53,7 +61,7 @@ export const projectsData = [
     image: "/background/dinner.png",
   },
   {
-    id: 7,
+    id: 8,
     name: "Bird Clash",
     description:
       "Adventurous and user interactive game made with C# and Unity Gaming Engine with camera focus rendering, unique design and different levels.",
@@ -62,7 +70,7 @@ export const projectsData = [
     image: "/background/birdclash.png",
   },
   {
-    id: 8,
+    id: 9,
     name: "Ssk-AiBot",
     description:
       "Made with Huggingface and AssemblyAI integrated with the Meta LLM using refined query to give the best answers to the users",
@@ -71,13 +79,21 @@ export const projectsData = [
     image: "/background/ssk-aibot.png",
   },
   {
-    id: 9,
+    id: 10,
     name: "Ball Jumper 3D",
     description:
       "This is my personal portfolio website developed using React, Tailwind CSS, and Next.js. It showcases my skills, projects, and achievements.",
     date: "2024-10-19",
     link: "https://ssk-portfolio.vercel.app/",
     image: "/background/ball.png",
+  },
+  {
+    id: 11,
+    name: "AI-VoiceProgrammerPro",
+    description:"VoiceGrammerPro is an AI-powered voice assistant that transcribes spoken English, analyzes grammar, and provides real-time grammar corrections using deep learning models Speech-to-text transcription using Whisper ,Grammar correction powered by Transformer-based models, Real-time or batch mode support, Web interface (Streamlit) Easy deployment with Docker.",
+    date: "2025-10-05",
+    link: "https://github.com/NITsush45/voicegrammerpro",
+    image: "/background/AI-Vo.png",
   }
 ];
 export const BtnList = [
